22411a86a8f4410a635fcf93a2fdd71aee4bdb18
Spring-Mirage <spring-mirage>
2024-11-01 18:35:41 -0500

    Feat: Add and Delete peer

--- client/src/context/RoomContext.tsx
+++ client/src/context/RoomContext.tsx
@@ -1,9 +1,11 @@
 
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useReducer, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { io as socketIOClient } from 'socket.io-client';
 import Peer from 'peerjs';
 import { v4 as uuidv4 } from 'uuid';
+import { peersReducer } from './PeerReducer';
+import { addPeerAction, removePeerAction } from './PeerActions';
 
 const WS = ('http://localhost:6001');
 
@@ -15,21 +17,27 @@ export const RoomContext = createContext<null | any>(null);
 
 const ws = socketIOClient(WS);
 
-export const RoomProvider: React.FC<RoomProviderProps> = ({children}) => {
+export const RoomProvider: React.FC<RoomProviderProps> = ({ children }) => {
     
     const navigate = useNavigate();
 
     const [me, setMe] = useState<Peer>();
 
     const [stream, setStream] = useState<MediaStream>();
+    
+    const [peers, dispatch] = useReducer(peersReducer, {})
 
-    const enterRoom = ({ roomId }: { roomId: "string"}) => {
+    const enterRoom = ({ roomId }: { roomId: string}) => {
         console.log({roomId});
         navigate(`/room/${roomId}`);
     }
 
     const getUsers = ({ participants }: { participants: string[]}) => {
         console.log({participants});
+    }
+
+    const removePeer = (peerId: string) => {
+        dispatch(removePeerAction(peerId));
     }
 
     useEffect(() => {
@@ -39,7 +47,7 @@ export const RoomProvider: React.FC<RoomProviderProps> = ({children}) => {
         setMe(peer);
 
         try {
-            navigator.mediaDevices.getUserMedia({video: true, audio: true})
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((stream) => {
                 setStream(stream);
             })
@@ -49,23 +57,35 @@ export const RoomProvider: React.FC<RoomProviderProps> = ({children}) => {
 
         ws.on('room-created', enterRoom);
         ws.on('get-users', getUsers);
+        ws.on('user-disconnected', removePeer);
         
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
         if(!me) return;
         if(!stream) return;
+
         ws.on('user-joined', ({peerId}) => {
             const call = me.call(peerId, stream);
+            call.on('stream',(peerStream) => {
+                dispatch(addPeerAction(peerId, peerStream));
+            })
         });
 
         me.on('call', (call) => {
             call.answer(stream);
+            call.on('stream',(peerStream) => {
+                dispatch(addPeerAction(call.peer, peerStream));
+            })
         });
         
     }, [me, stream]);
+
+    console.log({ peers })
+
     return (
-        <RoomContext.Provider value={{ws, me, stream}}>
+        <RoomContext.Provider value={{ws, me, stream, peers}}>
             {children}
         </RoomContext.Provider>
     )
--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -2,11 +2,12 @@ import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import { RoomContext } from "../context/RoomContext";
 import { VideoPlayer } from "../components/VideoPlayer";
+import { PeerState } from "../context/PeerReducer";
 
 export const Room = () => {
 
   const { id } = useParams();
-  const { ws, me, stream } = useContext(RoomContext)
+  const { ws, me, stream, peers } = useContext(RoomContext)
   useEffect(() => {
     if(me) ws.emit("join-room", {roomId: id, peerId: me._id})
   }, [id, me, ws])
@@ -15,8 +16,13 @@ export const Room = () => {
   return (
     <div>
       Room id: {id}
-      <div>
+      <div className="grid grid-cols-4 gap-4">
         <VideoPlayer stream={stream}/>
+        { Object.values(peers as PeerState).map((peer) => {
+          return (
+            <VideoPlayer stream={peer.stream} />
+          )
+        })}
       </div>
     </div>
   )
